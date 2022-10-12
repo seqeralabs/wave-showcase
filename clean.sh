9d125f5c9ac09041afc21468e8c210fb58d7629d
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-10-12 21:21:04 +0000

    Update clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,11 @@
+docker-rmi wave
+docker-rmi pditommaso
+docker-cleanup
+docker logout
+
 find . -name work | xargs  rm -rf 
 find . -name results | xargs  rm -rf 
 find . -name '.nextflow*' | xargs rm -rf
+
+git reset --hard origin/master
+
