aeefbff322760f57f324d21dd36350b1dd7b982b
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-04-30 13:38:49 +0000

    Bump use of freeze mode
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- example7/run.sh
+++ example7/run.sh
@@ -1,6 +1,6 @@
 nextflow run nf-core/rnaseq \
-	-profile conda,test \
-        -r 3.9 \
+	-profile test \
+        -r 3.14.0 \
 	--outdir s3://nextflow-ci/fusion-results \
         -w s3://nextflow-ci/test \
 	-bg $1 > pipeline.log & 
