087a463ffa7bb1658eaea457ed8bf6e9856706a6
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-11-02 09:24:49 +0000

    Bump rnaseq to version 3.17.0

--- example7/run.sh
+++ example7/run.sh
@@ -1,6 +1,6 @@
 nextflow run nf-core/rnaseq \
 	-profile test \
-        -r 3.15.1 \
+        -r 3.17.0 \
 	--outdir s3://nextflow-ci/fusion-results \
         -w s3://nextflow-ci/test \
 	-bg $1 > pipeline.log & 
